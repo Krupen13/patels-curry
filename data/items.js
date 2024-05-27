cf6d99230d4659c8df3a1518e308d08258cf348c
Krupen Patel <krupenpatel@Krupens-MBP.fritz.box>
2024-05-27 12:38:02 +0200

    Version1

---
+++ data/items.js
@@ -0,0 +1,176 @@
+const items = [
+  {
+    "id" : "1",
+    "image" : "/Users/krupenpatel/Documents/Patel's Curry/assets/Paatodi.png",
+    "name" : "Paatodi"
+  }, {
+    "price" : "699"
+  },
+
+  {
+    "id" : "2",
+    "image" : "/Users/krupenpatel/Documents/Patel's Curry/assets/PaneerAngara.png",
+    "name" : "Paneer Angara"
+  }, {
+    "price" : "1099"
+  },
+
+  {
+    "id" : "3",
+    "image" : "/Users/krupenpatel/Documents/Patel's Curry/assets/Dosa.png",
+    "name" : "Dosa"
+  }, {
+    "price" : "999"
+  },
+
+  {
+    "id" : "4",
+    "image" : "√/Users/krupenpatel/Documents/Patel's Curry/assets/Biryani.png",
+    "name" : "Hyderabadi Kulhad Biryani"
+  }, {
+    "price" : "899"
+  },
+
+  {
+    "id" : "5",
+    "image" : "/Users/krupenpatel/Documents/Patel's Curry/assets/Idlivada.png",
+    "name" : "Idli Meduvada Sambar"
+  }, {
+    "price" : "999"
+  },
+
+  {
+    "id" : "6",
+    "image" : "/Users/krupenpatel/Documents/Patel's Curry/assets/PaneerKadhai.png",
+    "name" : "Paneer Kadhai"
+  }, {
+    "price" : "799"
+  },
+
+  {
+    "id" : "7",
+    "image" : "/Users/krupenpatel/Documents/Patel's Curry/assets/PaneerPasandaRice.png",
+    "name" : "Paneer Pasanda"
+  }, {
+    "price" : "799"
+  },
+
+  {
+    "id" : "8",
+    "image" : "/Users/krupenpatel/Documents/Patel's Curry/assets/PaneerTikkaMasaala.png",
+    "name" : "Paneer Tikka Masaala"
+  }, {
+    "price" : "799"
+  },
+
+  {
+    "id" : "9",
+    "image" : "/Users/krupenpatel/Documents/Patel's Curry/assets/PaalakPaneer.png",
+    "name" : "Paalak Paneer"
+  }, {
+    "price" : "799"
+  },
+  {
+    "id" : "10",
+    "image" : "/Users/krupenpatel/Documents/Patel's Curry/assets/PaneerTandoori.png",
+    "name" : "Paneer Tandoori"
+  }, {
+    "price" : "899"
+  },
+  
+  {
+    "id" : "11",
+    "image" : "/Users/krupenpatel/Documents/Patel's Curry/assets/PaneerLababdar.png",
+    "name" : "Paneer Lababdar"
+  }, {
+    "price" : "899"
+  },
+
+  {
+    "id" : "12",
+    "image" : "/Users/krupenpatel/Documents/Patel's Curry/assets/BanarasiPaan.png",
+    "name" : "Banarasi Paan"
+  }, {
+    "price" : "299"
+  },
+
+  {
+    "id" : "13",
+    "image" : "/Users/krupenpatel/Documents/Patel's Curry/assets/icecream.png",
+    "name" : "Rajasthani Kulfi Ice-cream"
+  }, {
+    "price" : "549"
+  },
+
+  {
+    "id" : "14",
+    "image" : "/Users/krupenpatel/Documents/Patel's Curry/assets/Thali.png",
+    "name" : "56 Bhog"
+  }, {
+    "price" : "1599"
+  },
+
+  {
+    "id" : "15",
+    "image" : "/Users/krupenpatel/Documents/Patel's Curry/assets/BhindiFry.png",
+    "name" : "Bhindi Fry"
+  }, {
+    "price" : "799"
+  },
+
+  {
+    "id" : "16",
+    "image" : "/Users/krupenpatel/Documents/Patel's Curry/assets/Mukhvas.png",
+    "name" : "Mukhvaas"
+  }, {
+    "price" : "199"
+  },
+
+  {
+    "id" : "17",
+    "image" : "/Users/krupenpatel/Documents/Patel's Curry/assets/MangoLassi.png",
+    "name" : "Mango Lassi"
+  }, {
+    "price" : "299"
+  },
+
+  {
+    "id" : "18",
+    "image" : "/Users/krupenpatel/Documents/Patel's Curry/assets/NimbuPaani.png",
+    "name" : "Nimbu Paani"
+  }, {
+    "price" : "199"
+  },
+
+  {
+    "id" : "19",
+    "image" : "/Users/krupenpatel/Documents/Patel's Curry/assets/KulhadChai.png",
+    "name" : "Kulhad Chai"
+  }, {
+    "price" : "299"
+  },
+
+  {
+    "id" : "20",
+    "image" : "/Users/krupenpatel/Documents/Patel's Curry/assets/Pannakkam.png",
+    "name" : "Pannakkam"
+  }, {
+    "price" : "399"
+  },
+  {
+    "id" : "21",
+    "image" : "/Users/krupenpatel/Documents/Patel's Curry/assets/Dhokla.png",
+    "name" : "Dhokla"
+  }, {
+    "price" : "699"
+  },
+  
+  {
+    "id" : "22",
+    "image" : "/Users/krupenpatel/Documents/Patel's Curry/assets/PaaniPuri.png",
+    "name" : "Paani Puri"
+  }, {
+    "price" : "799"
+  }
+
+];
---
+++ pcApp.js
@@ -0,0 +1,109 @@
+
+
+let fname = document.getElementById("fname");
+let lname = document.getElementById("lname");
+let phone = document.getElementById("phone");
+let guest = document.getElementById("guest");
+let date = document.getElementById("date");
+let time = document.getElementById("time");
+
+let flag = 1;
+
+
+function validateForm() {
+
+  // var email = document.getElementById("mailaddress").value;
+
+   
+
+  
+
+  if(fname.value == "") {
+    document.getElementById("fnameError").innerHTML = "Please provide us your Firstname";
+    flag=0;
+  } else if(fname.value.length < 2) {
+    document.getElementById("fnameError").innerHTML = "Firstname requires at least 2 characters";
+    flag=0;
+  } else {
+    document.getElementById("fnameError").innerHTML = "";
+    flag = 1;
+  }
+
+  if(lname.value == "") {
+    document.getElementById("lnameError").innerHTML = "Please provide us your Lastname";
+    flag=0;
+  } else if(lname.value.length < 2) {
+    document.getElementById("lnameError").innerHTML = "Lastname requires at least 2 characters";
+    flag=0;
+  } else {
+    document.getElementById("lnameError").innerHTML = "";
+    flag=1;
+  }
+ 
+  let email = document.getElementById("mailaddress");
+
+  var regx = /^([a-zA-Z0-9\._]+)@([a-zA-Z0-9])+.([a-z]+)(.[a-z]+)?$/;
+
+
+  if(email.value == "") {
+    document.getElementById("mailError").innerHTML = "Please enter an Email address";
+    flag = 0;
+  } else if(!regx.test(email.value)) {
+    document.getElementById("mailError").innerHTML = "Invalid Email address";
+    flag = 0;
+  } else {
+    document.getElementById("mailError").innerHTML = "";
+    flag = 1;
+  }
+
+
+  /*  if(email.value == "") {
+    document.getElementById("mailError").innerHTML = "You need to enter an Email address";
+    flag=0;
+  } else if(email.value.match(regx)) {
+    document.getElementById("mailError").innerHTML = "";
+    flag=1;
+  } else {
+    document.getElementById("mailError").innerHTML = "Invalid Email address";
+    flag=0;
+  }   */
+
+  if(phone.value == "") {
+    document.getElementById("phoneError").innerHTML = "Phone number is required";
+    flag=0;
+  } else if(phone.value.length < 10 || phone.value.length > 10) {
+    document.getElementById("phoneError").innerHTML = "Invalid Phone number";
+    flag=0;
+  } else {
+    document.getElementById("phoneError").innerHTML = "";
+    flag=1;
+  }
+
+  if(guest.value <= 0) {
+    document.getElementById("guestError").innerHTML = "Add at least 1 guest";
+    flag=0;
+  } else if(guest.value > 20) {
+    document.getElementById("guestError").innerHTML = "Please contact us if number of guest exceeds 20";
+    flag=0;
+  } else {
+    document.getElementById("guestError").innerHTML = "";
+    
+  }
+
+
+if(flag) {
+  return true;
+} else {
+  return false;
+}
+
+
+}
+
+
+
+
+
+
+
+
