a47a3aa8e0fb46b3132d7be483f6e84741cec041
Krupen13 <krupenghanshyambhai@gmail.com>
2024-05-28 23:43:11 +0200

    Use items.js file

--- data/items.js
+++ data/items.js
@@ -1,198 +1,177 @@
 const items = [
   {
     "id" : "1",
-    "image" : "/Users/krupenpatel/Documents/Patels-Curry/assets/Paatodi.png",
+    "image" : "assets/Paatodi.png",
     "name" : "Paatodi",
-    "classname": "frontptd"
-  }, {
+    "classname": "frontptd",
     "price" : "699"
-  },
-
-  {
-    "id" : "2",
-    "image" : "/Users/krupenpatel/Documents/Patels-Curry/assets/PaneerAngara.png",
-    "name" : "Paneer Angara",
-    "classname": "frontpanang"
-  }, {
-    "price" : "1099"
-  },
-
-  {
-    "id" : "3",
-    "image" : "/Users/krupenpatel/Documents/Patels-Curry/assets/Dosa.png",
-    "name" : "Dosa",
-    "classname": "frontdosa"
-  }, {
-    "price" : "999"
-  },
-
-  {
-    "id" : "4",
-    "image" : "/Users/krupenpatel/Documents/Patels-Curry/assets/Biryani.png",
-    "name" : "Kulhad Biryani",
-    "classname": "frontkbir"
-  }, {
-    "price" : "899"
-  },
-
-  {
-    "id" : "5",
-    "image" : "/Users/krupenpatel/Documents/Patels-Curry/assets/Idlivada.png",
-    "name" : "Idli Sambar",
-    "classname": "frontidli"
-  }, {
-    "price" : "999"
-  },
-
-  {
-    "id" : "6",
-    "image" : "/Users/krupenpatel/Documents/Patels-Curry/assets/PaneerKadhai.png",
-    "name" : "Paneer Kadhai",
-    "classname": "frontpankad"
-  }, {
-    "price" : "799"
-  },
-
-  {
-    "id" : "7",
-    "image" : "/Users/krupenpatel/Documents/Patels-Curry/assets/PaneerPasandaRice.png",
-    "name" : "Pasanda Rice",
-    "classname": "frontppr"
-  }, {
-    "price" : "799"
-  },
-
-  {
-    "id" : "8",
-    "image" : "/Users/krupenpatel/Documents/Patels-Curry/assets/PaneerTikkaMasaala.png",
-    "name" : "Tikka Masaala",
-    "classname": "frontptm"
-  }, {
-    "price" : "799"
-  },
-
-  {
-    "id" : "9",
-    "image" : "/Users/krupenpatel/Documents/Patels-Curry/assets/PaalakPaneer.png",
-    "name" : "Paalak Paneer",
-    "classname": "frontpalpan"
-  }, {
-    "price" : "799"
-  },
-  {
-    "id" : "10",
-    "image" : "/Users/krupenpatel/Documents/Patels-Curry/assets/PaneerTandoori.png",
-    "name" : "Paneer Tandoori",
-    "classname": "frontptan"
-  }, {
-    "price" : "899"
-  },
-  
-  {
-    "id" : "11",
-    "image" : "/Users/krupenpatel/Documents/Patels-Curry/assets/PaneerLababdar.png",
-    "name" : "Paneer Lababdar",
-    "classname": "frontpanlav"
-  }, {
-    "price" : "899"
-  },
-
-  {
-    "id" : "12",
-    "image" : "/Users/krupenpatel/Documents/Patels-Curry/assets/BanarasiPaan.png",
-    "name" : "Banarasi Paan",
-    "classname": "frontpaan"
-  }, {
-    "price" : "299"
-  },
-
-  {
-    "id" : "13",
-    "image" : "/Users/krupenpatel/Documents/Patels-Curry/assets/icecream.png",
-    "name" : "Rajasthani Kulfi Ice-cream",
-    "classname": "frontkulfi"
-  }, {
-    "price" : "549"
-  },
-
-  {
-    "id" : "14",
-    "image" : "/Users/krupenpatel/Documents/Patels-Curry/assets/Thali.png",
-    "name" : "56 Bhog",
-    "classname": "front56"
-  }, {
-    "price" : "1599"
-  },
-
-  {
-    "id" : "15",
-    "image" : "/Users/krupenpatel/Documents/Patels-Curry/assets/BhindiFry.png",
-    "name" : "Bhindi Fry",
-    "classname": "frontbf"
-  }, {
-    "price" : "799"
-  },
-
-  {
-    "id" : "16",
-    "image" : "/Users/krupenpatel/Documents/Patels-Curry/assets/Mukhvas.png",
-    "name" : "Raajsi Mukhvaas",
-    "classname": "frontmukh"
-  }, {
-    "price" : "199"
-  },
-
-  {
-    "id" : "17",
-    "image" : "/Users/krupenpatel/Documents/Patels-Curry/assets/MangoLassi.png",
-    "name" : "Mango Lassi",
-    "classname": "frontmanlas"
-  }, {
-    "price" : "299"
-  },
-
-  {
-    "id" : "18",
-    "image" : "/Users/krupenpatel/Documents/Patels-Curry/assets/NimbuPaani.png",
-    "name" : "Nimbu Paani",
-    "classname": "frontnim"
-  }, {
-    "price" : "199"
-  },
-
-  {
-    "id" : "19",
-    "image" : "/Users/krupenpatel/Documents/Patels-Curry/assets/KulhadChai.png",
-    "name" : "Kulhad Chai",
-    "classname": "frontchai"
-  }, {
-    "price" : "299"
-  },
-
-  {
-    "id" : "20",
-    "image" : "/Users/krupenpatel/Documents/Patels-Curry/assets/Pannakkam.png",
-    "name" : "Pannakkam",
-    "classname": "frontpanna"
-  }, {
-    "price" : "399"
   },
   {
     "id" : "21",
-    "image" : "/Users/krupenpatel/Documents/Patels-Curry/assets/Dhokla.png",
+    "image" : "assets/Dhokla.png",
     "name" : "Dhokla",
-    "classname": "frontdkl"
-  }, {
+    "classname": "frontdkl",
     "price" : "699"
   },
   
   {
     "id" : "22",
-    "image" : "/Users/krupenpatel/Documents/Patels-Curry/assets/PaaniPuri.png",
+    "image" : "assets/PaaniPuri.png",
     "name" : "Paani Puri",
-    "classname": "frontpp"
-  }, {
+    "classname": "frontpp",
     "price" : "799"
+  },
+
+  {
+    "id" : "2",
+    "image" : "assets/PaneerAngara.png",
+    "name" : "Paneer Angara",
+    "classname": "frontpanang",
+    "price" : "1099"
+  }, 
+  {
+    "id" : "10",
+    "image" : "assets/PaneerTandoori.png",
+    "name" : "Paneer Tandoori",
+    "classname": "frontptan",
+    "price" : "899"
+  },
+  {
+    "id" : "3",
+    "image" : "assets/Dosa.png",
+    "name" : "Dosa",
+    "classname": "frontdosa",
+    "price" : "999"
+  },
+
+  {
+    "id" : "4",
+    "image" : "assets/Biryani.png",
+    "name" : "Kulhad Biryani",
+    "classname": "frontkbir",
+    "price" : "899"
+  },
+
+  {
+    "id" : "5",
+    "image" : "assets/Idlivada.png",
+    "name" : "Idli Sambar",
+    "classname": "frontidli",
+    "price" : "999"
+  },
+
+  {
+    "id" : "6",
+    "image" : "assets/PaneerKadhai.png",
+    "name" : "Paneer Kadhai",
+    "classname": "frontpankad",
+    "price" : "799"
+  },
+
+  {
+    "id" : "7",
+    "image" : "assets/PaneerPasandaRice.png",
+    "name" : "Pasanda Rice",
+    "classname": "frontppr",
+    "price" : "799"
+  },
+
+  {
+    "id" : "8",
+    "image" : "assets/PaneerTikkaMasaala.png",
+    "name" : "Tikka Masaala",
+    "classname": "frontptm",
+    "price" : "799"
+  },
+
+  {
+    "id" : "9",
+    "image" : "assets/PaalakPaneer.png",
+    "name" : "Paalak Paneer",
+    "classname": "frontpalpan",
+    "price" : "799"
+  },
+  
+  
+  {
+    "id" : "11",
+    "image" : "assets/PaneerLababdar.png",
+    "name" : "Paneer Lababdar",
+    "classname": "frontpanlav",
+    "price" : "899"
+  },
+  {
+    "id" : "14",
+    "image" : "assets/Thali.png",
+    "name" : "56 Bhog",
+    "classname": "front56",
+    "price" : "1599"
+  },
+
+  {
+    "id" : "15",
+    "image" : "assets/BhindiFry.png",
+    "name" : "Bhindi Fry",
+    "classname": "frontbf",
+    "price" : "799"
+  },
+
+  {
+    "id" : "17",
+    "image" : "assets/MangoLassi.png",
+    "name" : "Mango Lassi",
+    "classname": "frontmanlas",
+    "price" : "299"
+  },
+
+  {
+    "id" : "18",
+    "image" : "assets/NimbuPaani.png",
+    "name" : "Nimbu Paani",
+    "classname": "frontnim",
+    "price" : "199"
+  },
+
+  {
+    "id" : "19",
+    "image" : "assets/KulhadChai.png",
+    "name" : "Kulhad Chai",
+    "classname": "frontchai",
+    "price" : "299"
+  },
+
+  {
+    "id" : "20",
+    "image" : "assets/Pannakkam.png",
+    "name" : "Pannakkam",
+    "classname": "frontpanna",
+    "price" : "399"
+  },
+
+  {
+    "id" : "13",
+    "image" : "assets/icecream.png",
+    "name" : "Rajasthani Kulfi Ice-cream",
+    "classname": "frontkulfi",
+    "price" : "549"
+  },
+  
+
+  {
+    "id" : "12",
+    "image" : "assets/BanarasiPaan.png",
+    "name" : "Banarasi Paan",
+    "classname": "frontpaan",
+    "price" : "299"
+  },
+  
+
+  {
+    "id" : "16",
+    "image" : "assets/Mukhvas.png",
+    "name" : "Raajsi Mukhvaas",
+    "classname": "frontmukh",
+    "price" : "199"
   }
 
 ];
--- scripts/pcApp.js
+++ scripts/pcApp.js
@@ -1,159 +1,31 @@
-const items = [{
-  image: 'assets/Paatodi.png',
-  name: 'Paatodi',
-  price: 699,
-  classname: 'frontptd'
-},
-{
-  image: 'assets/Dhokla.png',
-  name: 'Dhokla',
-  price: 699,
-  classname: 'frontdkl'
-},
-{
-  image: 'assets/PaaniPuri.png',
-  name: 'Paani Puri',
-  price: 799,
-  classname: 'frontpp'
-}, 
-{
-  image: 'assets/PaneerTandoori.png',
-  name: 'Paneer Tandoori',
-  price: 899
-},
-{
-  image: 'assets/Dosa.png',
-  name: 'Dosa',
-  price: 999,
-  classname: 'frontdosa'
-},
-{
-  image: 'assets/Idlivada.png',
-  name: 'Idli Sambar',
-  price: 999,
-  classname: 'frontidli'
-},
-{
-  image: 'assets/BhindiFry.png',
-  name: 'Bhindi Fry',
-  price: 799,
-  classname: 'frontbf'
-}, 
-{
-  image: 'assets/Biryani.png',
-  name: 'Kulhad Biryani',
-  price: 899,
-  classname: 'frontkbir'
-},
-{
-  image: 'assets/PaneerPasandaRice.png',
-  name: 'Pasanda Rice',
-  price: 799,
-  classname: 'frontppr'
-},
-{
-  image: 'assets/PaalakPaneer.png',
-  name: 'Paalak Paneer',
-  price: 799,
-  classname: 'frontpalpan'
-},
-{
-  image: 'assets/PaneerAngara.png',
-  name: 'Paneer Angara',
-  price: 1099,
-  classname: 'frontpanang'
-},
-{
-  image: 'assets/PaneerKadhai.png',
-  name: 'Paneer Kadhai',
-  price: 799,
-  classname: 'frontpankad'
-},
-{
-  image: 'assets/PaneerLababdar.png',
-  name: 'Paneer Lababdaar',
-  price: 899,
-  classname: 'frontpanlav'
-},
-{
-  image: 'assets/PaneerTikkaMasaala.png',
-  name: 'Tikka Masaala',
-  price: 799,
-  classname: 'frontptm'
-},
-{
-  image: 'assets/Thali.png',
-  name: '56 Bhog',
-  price: 1599,
-  classname: 'front56'
-},
-{
-  image: 'assets/MangoLassi.png',
-  name: 'Mango Lassi',
-  price: 299,
-  classname: 'frontmanlas'
-},
-{
-  image: 'assets/NimbuPaani.png',
-  name: 'Nimbu Paani',
-  price: 199,
-  classname: 'frontnim'
-},
-{
-  image: 'assets/KulhadChai.png',
-  name: 'Kulhad Chai',
-  price: 299,
-  classname: 'frontchai'
-},
-{
-  image: 'assets/Pannakkam.png',
-  name: 'Pannakkam',
-  price: 399,
-  classname: 'frontpanna'
-},
-{
-  image: 'assets/icecream.png',
-  name: 'Rajasthani Kulfi Icecream',
-  price: 549,
-  classname: 'frontkulfi'
-},
-{
-  image: 'assets/BanarasiPaan.png',
-  name: 'Banarasi Paan',
-  price: 299,
-  classname: 'frontpaan'
-},
-{
-  image: 'assets/Mukhvas.png',
-  name: 'Raajsi Mukhvaas',
-  price: 199,
-  classname: 'frontmukh'
-}];
+
 
 let itemsHTML = '';
 
 items.forEach((item) => {
 
   itemsHTML += `
-  <div class="container mb-8 mr-16 duration-1000 ease-in-out hover:scale-110">
+  <div class="pr-10">
+  <div class="container  mb-8 mr-16 duration-1000 ease-in-out hover:scale-110">
   <div class=" card">
     
-    <div class="frontptd">
+    <div class="frontptd ">
 
       <img class="w-full h-full" src="${item.image}">
     
 
       
-      <h2 class="ml-2 text-2xl dishname">${item.name}&nbsp;€${item.price / 100}</h2>
+      <h2 class="ml-2 text-xl dishname">${item.name}&nbsp;€${item.price / 100}</h2>
       
-      <p  class="float-right max-sm:text-xs max-md:text-md -mt-7">
-        <button  class="w-8 h-6 pl-2 pr-2 mr-2 text-sm font-extrabold text-black bg-green-300 border-2 border-white rounded-md shadow-2xl cart active:bg-red-200 active:text-slate-950 visited:bg-black visited:text-bg-400 hover:bg-rose-600 hover:text-white hover:scale-110 hover:border-green-600">+<div class="tooltip">Add to cart</div></button>
+      <p  class="float-right -mt-7">
+        <button  class="w-8 h-8 pl-2 pr-2 mr-2 text-lg font-extrabold text-black bg-green-300 border-2 border-white rounded-md shadow-2xl cart active:bg-red-200 active:text-slate-950 visited:bg-black visited:text-bg-400 hover:bg-rose-600 hover:text-white hover:scale-110 hover:border-green-600">+<div class="tooltip">Add to cart</div></button>
         
       </p>
     
       
         </div>
   </div>
+</div>
 </div>
   `;
 
