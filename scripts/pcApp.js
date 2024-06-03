67a6a3aea61d051cdd72401a687b11016c220445
Krupen13 <krupenghanshyambhai@gmail.com>
2024-06-03 18:30:42 +0200

    added cart quantity number on the order.html

--- scripts/pcApp.js
+++ scripts/pcApp.js
@@ -1,8 +1,8 @@
-import {cart, addToCart} from '../data/cart.js';
+import {cart, addToCart, saveToStorage} from '../data/cart.js';
 
 import {items} from '../data/items.js';
 
-import { currencyRound} from './utilities/currency.js';
+import {currencyRound} from './utilities/currency.js';
 
 let itemsHTML = '';
 
@@ -20,7 +20,7 @@ items.forEach((item) => {
       <h2 class="ml-2 text-xl dishname">${item.name}&nbsp;€${currencyRound(item.price)}</h2>
       
       <p  class="float-right -mt-7">
-        <button data-item-id="${item.id}" class="w-10  js-add-to-cart h-8 pl-2 pr-2 mr-2 text-lg font-extrabold text-black bg-green-300 border-2 border-white rounded-md shadow-2xl cart active:bg-red-300 active:text-slate-950 visited:bg-black visited:text-bg-400 hover:bg-rose-600 hover:text-white hover:scale-110 hover:border-green-600">+<div class="tooltip">Add to cart</div></button>
+        <button data-item-id="${item.id}" class="w-10  js-add-to-cart h-8 pl-2 pr-2 mr-2 text-lg font-extrabold text-black bg-green-300 border-2 border-white rounded-md shadow-2xl cart  active:bg-red-300 active:text-slate-950 visited:bg-black visited:text-bg-400 hover:bg-rose-600 hover:text-white hover:scale-110 hover:border-green-600">+<div class="tooltip">Add to cart</div></button>
         
       </p>
 
@@ -36,18 +36,28 @@ document.querySelector('.js-items-grid').innerHTML = itemsHTML ;
 
  function updateCartQuantity() {
   let cartQuantity = 0;
+
+  if(cart.length === 0) {
+    document.querySelector('.js-cart-total').innerHTML = '0';
+  } else {
+    document.querySelector('.js-cart-total').innerHTML = cartQuantity;
+
+  }
+
   cart.forEach((cartItem) => {
     cartQuantity += cartItem.quantity;
   });  
 
-  document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+  document.querySelector('.js-cart-total').innerHTML = cartQuantity;
+  
 }
+
 
 document.querySelectorAll('.js-add-to-cart').forEach((button) => {
   button.addEventListener('click', () => {
     const itemId = button.dataset.itemId;
     addToCart(itemId);
-    updateCartQuantity();
+    updateCartQuantity(itemId);
 
   });
 
